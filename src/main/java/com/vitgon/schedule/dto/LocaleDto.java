bdf4c8afc77d17ee09b2c2d76d3210eaab62123e
Vitaliy Goncharov <www.devvit@gmail.com>
2019-03-16 23:50:18 +1000

    Added test for LocaleMapperService

--- src/main/java/com/vitgon/schedule/dto/LocaleDto.java
+++ src/main/java/com/vitgon/schedule/dto/LocaleDto.java
@@ -1,10 +1,12 @@
 package com.vitgon.schedule.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class LocaleDto {
 	private int id;
 	private String code;
