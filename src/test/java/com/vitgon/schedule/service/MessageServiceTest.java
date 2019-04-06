acf95b8a6d212f5c49312cc5fe6b4ec7f74f2848
Vitaliy Goncharov <www.devvit@gmail.com>
2019-04-07 01:28:18 +1000

    Create test for MessageServiceTest

---
+++ src/test/java/com/vitgon/schedule/service/MessageServiceTest.java
@@ -0,0 +1,44 @@
+package com.vitgon.schedule.service;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.MessageSource;
+
+public class MessageServiceTest {
+
+	private MessageSource messageSource;
+	private MessageService messageService;
+	
+	@Before
+	public void init() {
+		messageSource = mock(MessageSource.class);
+		messageService = new MessageService(messageSource);
+	}
+	
+	@Test
+	public void testGetMessageMethodWithCodeAndRequest() {
+		Locale locale = new Locale("en");
+		String code = "login";
+		HttpServletRequest request = spy(HttpServletRequest.class);
+		HttpSession session = spy(HttpSession.class);
+		
+		when(request.getSession()).thenReturn(session);
+		when(session.getAttribute("URL_LOCALE_ATTRIBUTE_NAME")).thenReturn(locale);
+		when(messageSource.getMessage(code, null, locale)).thenReturn("логин");
+		
+		String messageTranslation = messageService.getMessage(code, request);
+		Assert.assertEquals("логин", messageTranslation);
+		verify(messageSource).getMessage(code, null, locale);
+	}
+}
