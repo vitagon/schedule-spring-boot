2b39d3d845fd90942e73f128f21398e8ed70dd90
Vitaliy Goncharov <www.devvit@gmail.com>
2019-05-24 02:51:14 +1000

    Move showValidationErrors method to validation.js

--- src/main/resources/static/vue/admin-panel/majors-tab/remove-major.js
+++ src/main/resources/static/vue/admin-panel/majors-tab/remove-major.js
@@ -1,6 +1,6 @@
 import {serverBus} from '/vue/admin-panel/admin-panel.js';
-import {clearValidationMessages} from '/vue/util/validation.js';
-import {getErrorMessage, showValidationErrors} from '/vue/util/error-msg-builder.js'
+import {clearValidationMessages, showValidationErrors} from '/vue/util/validation.js';
+import {getErrorMessage} from '/vue/util/error-msg-builder.js'
 
 Vue.component('remove-major', {
 	data: function () {
--- src/main/resources/static/vue/util/validation.js
+++ src/main/resources/static/vue/util/validation.js
@@ -1,3 +1,19 @@
 export function clearValidationMessages($form) {
 	$form.find('.validation-message').remove();
 }
+
+export function showValidationErrors(errors, $form) {
+	$form.find('.validation-message').remove();
+	$.each(errors, function (fieldName, fieldErrors) {
+		let $field = $form.find('[name=' + fieldName + ']');
+		
+		if ($field != null) {
+			let $fieldWrap = $field.closest('div');
+			$.each(fieldErrors, function (index, fieldError) {
+				$fieldWrap.append('<div class="validation-message" style="color: red">' +
+								  fieldError +
+								  '</div>');
+			});
+		}
+	});
+}
